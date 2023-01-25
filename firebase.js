331ceb6cdef8cdc19b8cfc00590b8835681b8d16
Aymen <aymenbouazra994@gmail.com>
2023-01-25 17:32:54 +0100

    add todo design

---
+++ firebase.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBjFajON9sNyOCH_SR36wz1i7eiDA-mvSA",
+  authDomain: "crud-app-react-native-6c738.firebaseapp.com",
+  projectId: "crud-app-react-native-6c738",
+  storageBucket: "crud-app-react-native-6c738.appspot.com",
+  messagingSenderId: "724236823759",
+  appId: "1:724236823759:web:172a0ee92ddb0f1cbb5bba"
+};
+
+const app = initializeApp(firebaseConfig);
+const CrudApp = fireS
--- screens/AddTodo.js
+++ screens/AddTodo.js
@@ -1,11 +1,33 @@
-import React from 'react'
-import { SafeAreaView, StyleSheet, Text } from 'react-native'
+import React, { useState } from 'react'
+import { SafeAreaView, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native'
 
 const AddTodo = () => {
+    const [form,setForm] = useState({
+        title:'',
+        description:''
+    });
+    const handleChange = (value) => {
+        setForm({...form,[name]:value})
+        console.log(form);
+    }
     return (
         <SafeAreaView style={styles.container}>
+            <View style={styles.formGroup}>
+                <View style={styles.inputGroup}>
+                    <Text style={styles.title}>Name:</Text>
+                    <TextInput style={styles.input} onChangeText={(text)=>handleChange(text)} name='title' placeholder='Task1' />
+                </View>
+                <View style={styles.inputGroup}>
+                    <Text style={styles.title}>Description:</Text>
+                    <TextInput style={styles.input} onChangeText={(text)=>handleChange(text)} name='description' placeholder='Description' />
+                </View>
+                <View style={styles.buttonContainer}>
+                    <TouchableOpacity style={styles.button}>
+                        <Text style={styles.buttonText}> Create </Text>
+                    </TouchableOpacity>
+                </View>
+            </View>
 
-            <Text>Add to do </Text>
         </SafeAreaView>
     )
 }
@@ -13,9 +35,48 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
+
     },
+    header: {
+        width: '100%',
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    formGroup: {
+        marginHorizontal: 30,
+    },
+    inputGroup: {
+        paddingTop: 20
+    },
+    title: {
+        marginVertical: 10
+    },
+    input: {
+        borderWidth: 1,
+        borderColor: '#ccc',
+        borderRadius: 10,
+        paddingHorizontal: 10,
+        paddingVertical: 10,
+    },
+    buttonContainer: {
+        width: '100%',
+        justifyContent: 'center',
+        alignItems: 'center',
+        paddingVertical: 20
+    },
+    button: {
+        width: '30%',
+        backgroundColor: '#ac3',
+        borderRadius: 10,
+        paddingHorizontal: 20,
+        paddingVertical: 5,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    buttonText: {
+        color: 'white'
+    },
+
 });
 
 export default AddTodo
